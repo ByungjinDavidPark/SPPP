9da78932a9c85138edef7f8bee79f509526f9d68
davidpark <byungjin.david.park@gmail.com>
2016-05-25 15:20:05 +1200

    Update
    sort contextobject

--- SmartPeerPathwayPlanner/AppDelegate.m
+++ SmartPeerPathwayPlanner/AppDelegate.m
@@ -181,9 +181,8 @@ courseAim:(NSString *)courseAim
          Lightweight migration will only work for a limited set of schema changes; consult "Core Data Model Versioning and Data Migration Programming Guide" for details.
          
          */
-        NSLog(@"Unresolved error %@, %@", error, [error userInfo]);
-        abort();
         //NSLog(@"Unresolved error %@, %@", error, [error userInfo]);
+        //abort();
     }
     
     return _persistentStoreCoordinator;
